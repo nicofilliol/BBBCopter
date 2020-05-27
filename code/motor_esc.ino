d40e65c768f2ce92755fb6e216d0b78be048b96b
nicofilliol <nicolas.filliol@outlook.com>
2020-05-27 18:59:48 +0200

    updated naming style to google c++ standard

--- code/motor_esc.ino
+++ code/motor_esc.ino
@@ -1,19 +1,20 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-// set Pin numbers:
-const int PotentiometerPin = A0;
+// Set pin numbers:
+const int kPotentiometerPin = A0;
+const int kESCPin = 9;
 
 Servo ESC; // Create a servo object to control the ESC
 
 void setup() {
   // Put your setup code here, to run once:
-  ESC.attach(9,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+  ESC.attach(kESCPin,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
 }
 
 void loop() {
   // Put your main code here, to run repeatedly:
-   int potValue = analogRead(PotentiometerPin);   // Reads the value of the potentiometer (value between 0 and 1023)
+   int potValue = analogRead(kPotentiometerPin);   // Reads the value of the potentiometer (value between 0 and 1023)
    potValue = map(potValue, 0, 1023, 0, 180);   // Scale it for use with the servo library (value between 0 and 180, usually degrees for a servo motor -> corresponds to motor speed in this case)
    ESC.write(potValue); // Send the signal to the ESC
 }
